6d966f1c87859cbc2a229600c09aaa56f1c40e5f
1revolman1 <1revolman1@gmail.com>
2020-02-04 00:14:24 +0200

    add new component

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const userRouter = require("./routes/userRouter.js");
-// const homeRouter = require("./routes/homeRouter.js");
-
-// app.use("/users", userRouter);
-// app.use("/", homeRouter);
-
-// app.use(function(req, res, next) {
-//   res.status(404).send("Not Found");
-// });
-
-// app.listen(8080);
-
 const axios = require("axios");
 const express = require("express");
 const bodyParser = require("body-parser");
